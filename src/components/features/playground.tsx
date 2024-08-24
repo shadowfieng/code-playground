728fdf53025718bf8d5fd5c792674f5a27fdc6ad
Fedor Shadrin <f.shadrin@velvetech.dev>
2024-08-24 19:12:28 +0700

    feat: add resizable component

--- src/components/features/playground.tsx
+++ src/components/features/playground.tsx
@@ -1,9 +1,21 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup
+} from "../ui/resizable";
+
 export const Playground = () => {
   return (
     <div className="flex playground">
-      <div className="w-1/2">Editor</div>
-      <div className="h-full w-[2px] bg-slate-800 cursor-col-resize" />
-      <div className="w-1/2">Output</div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel>
+          <div>Editor</div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <div>Output</div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
